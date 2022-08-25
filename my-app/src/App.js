fb30bcbfb97788ccd01935d99f6c5583db0b8625
Lee-moran <leemoran06@hotmail.com>
2022-08-25 13:32:26 +0000

    useState Hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import ControlledFormHooks from './components/ControlledFormHooks';
 import HooksCounter from './components/HooksCounter';
+import UseStateWithArrays from './components/UseStateWithArrays';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 
 function App() {
   return (
     <div className="App">
       <HooksCounter/>
+      
+      <ControlledFormHooks/>
+
+      <UseStateWithArrays/>
+
+      <UseStateWithObjects/>
       
     </div>
   );
